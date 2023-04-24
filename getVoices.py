350fede28604d8fb8f5a40cda291a1b9ca6a092d
Brett Fitzgerald <brettgfitzgerald@gmail.com>
2023-04-24 16:28:05 -0400

    Get voice info

--- getVoices.py
+++ getVoices.py
@@ -1,10 +1,11 @@
 import requests
+import local_settings
 
 url = "https://api.elevenlabs.io/v1/voices"
 
 headers = {
 	"Accept": "application/json",
-	"xi-api-key": "<xi-api-key>"
+	"xi-api-key": local_settings.XI_API_KEY
 }
 
 response = requests.get(url, headers=headers)
